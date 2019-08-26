b21aa3551fb37f8400fe08a3906f0aeb4f06e3bf
fatima ahmed <fatymah001@gmail.com>
2019-08-27 01:28:37 +0300

    Complete test for RestClient

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def index
+    @name = "I am the Index action!"
+  end 
+
+  def show
+    @name = "I am the show action"
+  end
+
+  def new
+    @name = "I am the new action"
+  end
+
+  def edit
+    @name = "I am the edit action"
+  end
+
+  def create
+    @name = "I am the create action"
+  end
+
+  def update
+    @name="I am the update action"
+  end
+
+  def destroy
+    @name="I am the destroy action"
+  end
+end
